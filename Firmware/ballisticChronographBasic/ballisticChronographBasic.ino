a188c4ca0ce3a5ca43413109b05e4e8ff3f587b7
td0g <vtgerritsen@gmail.com>
2020-07-26 07:20:11 -0600

    1.2
    Interrupt control improvements

--- Firmware/ballisticChronographBasic/ballisticChronographBasic.ino
+++ Firmware/ballisticChronographBasic/ballisticChronographBasic.ino
@@ -16,6 +16,9 @@ CHANGELOG:
     2020-06-16
     Switched from eRCaGuy library to directly accessing Timer1
     Using watchdog timer to catch timeouts - better performance
+  1.2
+    2020-07-26
+    Improvements to the interrupt disabling/enabling function
 
 LICENSE:
   This program is free software: you can redistribute it and/or modify
@@ -310,7 +313,9 @@ void loop(){
   Serial.print(objectSpeed);
   Serial.println(" fps)");
 
-
+//Wait until Serial is all finished
+  while ((UCSR0A & _BV (TXC0)) == 0){};
+    
 }
 
 
@@ -324,24 +329,53 @@ void enableInterrupts(){ editInterrupts(1);}  //Turn on Timer0 and Timer2
 
 
 void editInterrupts(byte _s){
-//We need to remember the states of the Timer0 and Timer2 registers before turning them off
+  //0 = All interrupts OFF
+  //1 = All interrupts ON
+  //2 = Turn on Serial
   static byte _tccr0b;
   static byte _tccr2b;
+  static byte _spcr;
+  static byte _ucsr0b;
+  static byte _adcsra;
+  static byte _adcsrb;
+  static byte _acsr;
+  static byte _eimsk;
   static byte _intState = 1;
-  if (_s != _intState){
-    if (!_s){
-//Turning timer prescalers OFF
+  if (!_s){
+    if (_intState){
       _intState = 0;
       _tccr0b = TCCR0B;
       _tccr2b = TCCR2B;
-      TCCR0B |= 0b11111000;
-      TCCR2B |= 0b11111000;
+      _spcr = SPCR;
+      _adcsra = ADCSRA;
+      _adcsrb = ADCSRB;
+      _acsr = ACSR;
+      _eimsk = EIMSK;
+      TCCR0B = 0;
+      TCCR2B = 0;
+      SPCR = 0;
+      ADCSRA = 0;
+      ADCSRB = 0;
+      ACSR = 0b10000000;
+      EIMSK = 0b00000000;
     }
-    else {
-//Resetting timer prescalers to previous state
-      _intState = 1;
-      TCCR0B = _tccr0b;
-      TCCR2B = _tccr2b;
+    if (UCSR0B){
+      _ucsr0b = UCSR0B;
+      UCSR0B = 0; //Need to reset this for the interrupt serial
     }
+  }
+  else if (_s == 1 && !_intState) {
+    _intState = 1;
+    TCCR0B = _tccr0b;
+    TCCR2B = _tccr2b;
+    SPCR = _spcr;
+    UCSR0B = _ucsr0b;
+    ADCSRA = _adcsra;
+    ADCSRB = _adcsrb;
+    ACSR = _acsr;
+    EIMSK = _eimsk;
+  }
+  else if (_s == 2 && !_intState){
+    if (!_intState) UCSR0B = _ucsr0b;
   }
 }
